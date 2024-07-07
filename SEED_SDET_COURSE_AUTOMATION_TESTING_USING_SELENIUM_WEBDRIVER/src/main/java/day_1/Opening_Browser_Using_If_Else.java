4e09ebad1e91e3a679bbafca482d7c4c0b7b9b3e
itsvishwa23 <itsvishwa23@gmail.com>
2024-07-07 12:01:13 +0530

    :tada:

--- SEED_SDET_COURSE_AUTOMATION_TESTING_USING_SELENIUM_WEBDRIVER/src/main/java/day_1/Opening_Browser_Using_If_Else.java
+++ SEED_SDET_COURSE_AUTOMATION_TESTING_USING_SELENIUM_WEBDRIVER/src/main/java/day_1/Opening_Browser_Using_If_Else.java
@@ -1,9 +1,50 @@
 package day_1;
 
+import java.util.Scanner;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Opening_Browser_Using_If_Else {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		int choice;
+		System.out.println("Enter your choice");
+		System.out.println("1: Firefox");
+		System.out.println("2: Chrome");
+		System.out.println("3: Edge");
+
+		Scanner sc = new Scanner(System.in);
+		choice = sc.nextInt();
+
+		if (choice == 1) {
+			WebDriver driver = new FirefoxDriver();
+			driver.get("https://www.google.com/");
+
+		} else if (choice == 2) {
+
+			WebDriver driver_2 = new ChromeDriver();
+			driver_2.get("https://www.google.com/");
+
+		}
+
+		else if (choice == 3) {
+
+			WebDriver driver_3 = new EdgeDriver();
+			driver_3.get("https://www.google.com/");
+
+		}
+		else {
+			System.err.println("Wrong Choice Entered");
+
+
+
+		}
+
 
 	}
 
--- SEED_SDET_COURSE_AUTOMATION_TESTING_USING_SELENIUM_WEBDRIVER/src/main/java/day_1/Opening_Browser_Using_Switch_Case.java
+++ SEED_SDET_COURSE_AUTOMATION_TESTING_USING_SELENIUM_WEBDRIVER/src/main/java/day_1/Opening_Browser_Using_Switch_Case.java
@@ -1,11 +1,45 @@
 package day_1;
 
+import java.util.Scanner;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Opening_Browser_Using_Switch_Case {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int choice;
+		System.out.println("Enter your choice");
+		System.out.println("1: Firefox");
+		System.out.println("2: Chrome");
+		System.out.println("3: Edge");
 
+		Scanner sc = new Scanner(System.in);
+		choice = sc.nextInt();
+
+		switch (choice) {
+		case 1:
+			WebDriver driver1 = new FirefoxDriver();
+			driver1.get("https://www.google.com/");
+			break;
+
+		case 2:
+			WebDriver driver2 = new ChromeDriver();
+			driver2.get("https://www.google.com/");
+			break;
+
+		case 3:
+			WebDriver driver3 = new EdgeDriver();
+			driver3.get("https://www.google.com/");
+			break;
+
+		default:
+			System.err.println("Wrong Choice Entered");
+		}
 	}
 
 }
