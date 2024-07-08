39c4e36b4e73b1843d40b6b397548fd35236181f
VISHWANATH TAWARE <108166846+itsvishwa23@users.noreply.github.com>
2024-07-08 11:50:54 +0530

    Update Find_Element_on_Google.java

--- SEED_SDET_COURSE_AUTOMATION_TESTING_USING_SELENIUM_WEBDRIVER/src/main/java/day_2/Find_Element_on_Google.java
+++ SEED_SDET_COURSE_AUTOMATION_TESTING_USING_SELENIUM_WEBDRIVER/src/main/java/day_2/Find_Element_on_Google.java
@@ -1,6 +1,7 @@
 package day_2;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -17,6 +18,7 @@ public class Find_Element_on_Google {
 		WebElement search = driver.findElement(By.className("gLFyf"));
 
 		search.sendKeys("Seed Infotech");
+		search.sendKeys(Keys.ENTER);
 //		search.submit();
 
 	}
