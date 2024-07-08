08d5fc35b45bf53a2855ce428032b65b6f047ce9
VISHWANATH TAWARE <108166846+itsvishwa23@users.noreply.github.com>
2024-07-08 11:12:10 +0530

    :tada:

--- SEED_SDET_COURSE_AUTOMATION_TESTING_USING_SELENIUM_WEBDRIVER/src/main/java/day_2/Find_Element_on_Google.java
+++ SEED_SDET_COURSE_AUTOMATION_TESTING_USING_SELENIUM_WEBDRIVER/src/main/java/day_2/Find_Element_on_Google.java
@@ -1,23 +1,24 @@
-
 package day_2;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class Find_Element_on_Orange_HRM {
+public class Find_Element_on_Google {
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 
 		WebDriver driver = new FirefoxDriver();
 		driver.manage().window().maximize();
-		driver.get("https://mail.rediff.com/cgi-bin/login.cgi");
+		driver.get("https://www.google.com");
 
-		driver.findElement(By.name("login")).sendKeys("Admin");
-		driver.findElement(By.name("passwd")).sendKeys("Admin");
-		driver.findElement(By.name("proceed")).click();
-		driver.close();
+		WebElement search = driver.findElement(By.className("gLFyf"));
+
+		search.sendKeys("Seed Infotech");
+		search.submit();
+
 	}
 
 }
