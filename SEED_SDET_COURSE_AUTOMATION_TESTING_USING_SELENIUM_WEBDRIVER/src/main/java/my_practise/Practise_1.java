3811c69a8bbf0921bf023fac2df425a939e5db81
itsvishwa23 <itsvishwa23@gmail.com>
2024-07-10 19:40:18 +0530

    :tada:

--- SEED_SDET_COURSE_AUTOMATION_TESTING_USING_SELENIUM_WEBDRIVER/src/main/java/my_practise/Practise_1.java
+++ SEED_SDET_COURSE_AUTOMATION_TESTING_USING_SELENIUM_WEBDRIVER/src/main/java/my_practise/Practise_1.java
@@ -1,10 +1,7 @@
 package my_practise;
 
-import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Practise_1 {
@@ -14,7 +11,9 @@ public class Practise_1 {
 
 		WebDriver driver = new FirefoxDriver();
 		driver.manage().window().maximize();
-		driver.get("https://demos.telerik.com/aspnet-ajax/imagegallery/");
+//		driver.get("https://testautomationpractice.blogspot.com/");
+		driver.get("https://demoqa.com/automation-practice-form");
+//		driver.get("https://mdbootstrap.com/docs/standard/extended/dropdown-checkbox/#");
 
 
 //		WebElement test = driver.findElement(By.id("ctl00_TripXpertLogo"));
@@ -22,25 +21,35 @@ public class Practise_1 {
 //		WebElement test = driver.findElement(By.linkText("Destinations"));
 //		WebElement test = driver.findElement(By.partialLinkText("Desti"));
 //		WebElement test = driver.findElement(By.name(""));
-		WebElement test = driver.findElement(By.tagName("div"));
+//		WebElement test = driver.findElement(By.tagName("div"));
+//		
+//
+//		if (test != null) {
+//
+//			System.out.println("Element found.");
+//
+//		} else {
+//			System.out.println("Element not found.");
+//
+//		}
+//
+//		
+//		List<WebElement> lst_2 = driver.findElements(By.tagName("div"));
+//		
+//		System.out.println(lst_2.size());
+
+//
+//		WebElement drpdwn = driver.findElement(By.xpath("//select[@id='country']"));
+//		Select dropdwn = new Select(drpdwn);
+//		dropdwn.selectByVisibleText("Germany");
+//		driver.close();
 		
-
-		if (test != null) {
-
-			System.out.println("Element found.");
-
-		} else {
-			System.out.println("Element not found.");
-
-		}
-
 		
-		List<WebElement> lst_2 = driver.findElements(By.tagName("div"));
-		
-		System.out.println(lst_2.size());
+//		
+		driver.findElement(By.xpath("//div[contains(text(),'Select State')]")).click();
+		driver.findElement(By.xpath("//*[@id=\"react-select-3-option-2\"]")).click();
 
 
-		driver.close();
 	}
 
 }
