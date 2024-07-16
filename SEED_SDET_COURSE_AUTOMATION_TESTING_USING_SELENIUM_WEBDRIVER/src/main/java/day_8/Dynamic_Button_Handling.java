a11681eab24ab1344272c48e6f708bfa7fb8c3d0
VISHWANATH TAWARE <108166846+itsvishwa23@users.noreply.github.com>
2024-07-16 09:56:16 +0530

    :tada:

---
+++ SEED_SDET_COURSE_AUTOMATION_TESTING_USING_SELENIUM_WEBDRIVER/src/main/java/day_8/Dynamic_Button_Handling.java
@@ -0,0 +1,27 @@
+package day_8;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Dynamic_Button_Handling {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3000));
+		driver.get("https://demoqa.com/buttons");
+		Actions act = new Actions(driver);
+		WebElement btn1 = driver.findElement(By.xpath("//button[@id='doubleClickBtn']"));
+		WebElement btn2 = driver
+				.findElement(By.xpath("/html[1]/body[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[2]/div[3]/button[1]"));
+		WebElement btn3 = driver.findElement(By.xpath("//button[@id='rightClickBtn']"));
+		act.doubleClick(btn1).perform();
+		act.click(btn2).perform();
+	}
+
+}
