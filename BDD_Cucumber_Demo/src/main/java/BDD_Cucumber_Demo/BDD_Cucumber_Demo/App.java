ac1e4669d8333efe38cbe720177555cf8971a013
VISHWANATH TAWARE <108166846+itsvishwa23@users.noreply.github.com>
2024-07-26 11:52:04 +0530

    :tada:

---
+++ BDD_Cucumber_Demo/src/main/java/BDD_Cucumber_Demo/BDD_Cucumber_Demo/App.java
@@ -0,0 +1,13 @@
+package BDD_Cucumber_Demo.BDD_Cucumber_Demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ BDD_Cucumber_Demo/src/test/resources/business_logic/f01_google.feature
@@ -0,0 +1,11 @@
+
+Feature: Google Application
+
+
+
+  Scenario: Test Google Home Page
+    Given open wwww.google.com application
+    When user captures google title
+    Then validate google appliation 
+  
+
